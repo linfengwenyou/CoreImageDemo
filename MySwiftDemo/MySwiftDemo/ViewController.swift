dcc8c2e3e0c529a2a98478846bffa1f6c6bc6120
lius <lawson_@sina.com>
2019-06-29 09:23:44 +0800

    update

--- MySwiftDemo/MySwiftDemo/ViewController.swift
+++ MySwiftDemo/MySwiftDemo/ViewController.swift
@@ -8,7 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDataSourcePrefetching {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+    
+    func downsample(imageAt imageURL:URL, to pointSize:CGSize, scale:CGFloat) -> UIImage {
+        let imageSourceOptions = [kCGImageSourceShouldCache: false] as CFDictionary
+        let imageSource = CGImageSourceCreateWithURL(imageURL as CFURL,imageSourceOptions)!
+        let maxDimensionInPixels = max(pointSize.width, pointSize.height) * scale
+        let downsampleOptions = [
+            kCGImageSourceCreateThumbnailFromImageAlways: true,
+            kCGImageSourceShouldCacheImmediately: true,
+            kCGImageSourceCreateThumbnailWithTransform: true,
+            kCGImageSourceThumbnailMaxPixelSize:maxDimensionInPixels
+        ] as CFDictionary
+        
+        let sourceSampledImage = CGImageSourceCreateThumbnailAtIndex(imageSource, 0, downsampleOptions)!
+        
+        return UIImage(cgImage: sourceSampledImage)
+    }
+    
+    
+    let searialQueue = DispatchQueue(label: "Decode queue")
+    func collectionView(_ collectionView: UICollectionView, prefetchItemsAt indexPaths: [IndexPath]) {
+        for indexPath in indexPaths {
+            searialQueue.async {
+                let downsampledImage = downsample("https://wwwwimage.png")
+                DispatchQueue.main.async {
+                    self.update()
+                }
+            }
+        }
+    }
+    
+    
+    
+    func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
