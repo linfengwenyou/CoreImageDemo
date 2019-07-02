da83a68dc43bc80ffc3b0949e32457b387163bac
lius <lawson_@sina.com>
2019-07-02 12:07:49 +0800

    update

--- MySwiftDemo/MySwiftDemo/ViewController.swift
+++ MySwiftDemo/MySwiftDemo/ViewController.swift
@@ -62,35 +62,62 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         self.view.backgroundColor = UIColor.white
 //        runloopMonitor()
         
-        let queue = DispatchQueue.init(label: "com.kang.song",attributes:[])
+//        let queue = DispatchQueue.init(label: "com.kang.song",attributes:DispatchQueue.Attributes.concurrent)
+        let queue = DispatchQueue.global(qos: DispatchQoS.background.qosClass)
 
         print("this is start", Thread.current)
 
+        //TODO: 可能图片顺序混乱
+        
         queue.sync {
             print("this is a sync test", Thread.current)
+            queue.async {
+                print("this is async test", Thread.current)
+            }
         }
+        
+        
+        
 
-        queue.async {
-            runloopMonitor()
-            let timer = Timer.scheduledTimer(withTimeInterval: 5.0, repeats: true, block: { (timer) in
-                print("timer value ---")
-            })
-            
-            RunLoop.current.add(timer, forMode: RunLoop.Mode.common)
-            
-            let timer1 = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { (timer) in
-                print("timer1 value ---")
-            })
-            
-            RunLoop.current.add(timer1, forMode: RunLoop.Mode.common)
-            
-            RunLoop.current.run(until: Date.init(timeInterval: 10, since: Date.init()))
-            
-            
-            print("this is a sync test", Thread.current)
-        }
+//        queue.async {
+//            runloopMonitor()
+//            let timer = Timer.scheduledTimer(withTimeInterval: 5.0, repeats: true, block: { (timer) in
+//                print("timer value ---")
+//            })
+//
+//            RunLoop.current.add(timer, forMode: RunLoop.Mode.common)
+//
+//            let timer1 = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { (timer) in
+//                print("timer1 value ---")
+//            })
+//
+//            RunLoop.current.add(timer1, forMode: RunLoop.Mode.common)
+//
+//            RunLoop.current.run(until: Date.init(timeInterval: 10, since: Date.init()))
+//
+//
+//            print("this is a sync test", Thread.current)
+//        }
 
         print("this is finish", Thread.current)
+        
+        let iamgeView = UIImageView.init(frame: self.view.bounds)
+        self.view.addSubview(iamgeView)
+        
+//        iamgeView.image = self.snapTwoImages(image1, image2: image2)
+        var images = [UIImage]()
+        for i in 0...1000 {
+            if let image = UIImage.init(named: "\(i)") {
+                images.append(image)                
+            }
+        }
+        
+        
+//        图片存储本地
+        queue.async {
+            self.snapMoreImages(images)
+        }
+        
     }
     
     
@@ -99,6 +126,73 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     }
     
     
+    func snapMoreImages(_ images:[UIImage]) {
+        // 开启上下文
+        var totalHeight = UIScreen.main.bounds.size.height * 40
+        let width = UIScreen.main.bounds.size.width
+        
+        
+        // 1. 先将数组内容进行子级分组
+        
+        var imageArrs = [[UIImage]]()
+        var totalHeights = [CGFloat]()
+        var origY:CGFloat = 0
+        var tmpImages = [UIImage]()
+        for image in images {
+            let height = width / image.size.width * image.size.height;
+            if height + origY > totalHeight {
+                imageArrs.append(tmpImages)
+                totalHeights.append(origY)
+                tmpImages = [UIImage]()
+                origY = 0
+            }
+            origY = origY + height
+            tmpImages.append(image)
+        }
+        
+        totalHeights.append(origY)
+        imageArrs.append(tmpImages)
+//        通过组的方式进行处理
+        
+//        let group = DispatchGroup.init()
+        let conQueue = DispatchQueue.init(label: "com.concurrent.song",  attributes: DispatchQueue.Attributes.concurrent)
+        
+        for (index, images) in imageArrs.enumerated() {
+            conQueue.async {
+                UIGraphicsBeginImageContext(CGSize.init(width: width, height: totalHeights[index]))
+                // 画图
+                var orighinY:CGFloat = 0
+                for image in images {
+                    let height = width / image.size.width * image.size.height;
+                    image.draw(in: CGRect.init(x: 0, y: orighinY, width: width, height: height))
+                    orighinY = height + orighinY
+                }
+                
+                let image = UIGraphicsGetImageFromCurrentImageContext()
+                UIGraphicsEndImageContext()
+                self.saveImage(image: image!, imageName: "testDemo\(index).jpg")
+            }
+            
+        }
+        
+      
+    }
+    
+    
+    func saveImage(image:UIImage, imageName:String) {
+        let path = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.cachesDirectory, FileManager.SearchPathDomainMask.userDomainMask, true).first!
+        
+        let fileName = path + "/" + imageName
+        print(fileName)
+        
+        // 写入路径
+        let isSuccess = FileManager.default.createFile(atPath: fileName, contents: image.jpegData(compressionQuality: 0.8))
+        if isSuccess {
+            print("文件写入成功")
+        } else {
+            print("文件写入失败")
+        }
+    }
     
 }
 
